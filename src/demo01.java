5dd28c42d9835e0b93631b0bc2aa31b9ffeda880
1998zdz <50400327+1998zdz@users.noreply.github.com>
2022-08-09 16:12:07 +0800

    pull commit

--- src/demo01.java
+++ src/demo01.java
@@ -5,5 +5,6 @@ public class demo01 {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
